37f1bcc835064b54def5294016531e38a3958ef9
Bluebar1 <yupnick123@gmail.com>
2021-11-02 13:01:31 -0400

    Offline Mode calculations
    Now calculates: Total Runtime(ms) & Avg Runtime(ms) & Total Faces Detected & Avg Faces Detected
    
    Then writes to data\results\latex.txt to be copy and pasted into Latex

--- config.py
+++ config.py
@@ -8,4 +8,5 @@ offlineDataLocation = 'data/results/offline.json'
 instantImageOutput = 'data/images/output/instant_result.jpg'
 liveImg = 'data/images/output/liveframe.jpg'
 offlineOutputPath = 'data/images/output/offline'
+offlineLatexLocation = 'data/results/latex.txt'
 
--- modes/offline.py
+++ modes/offline.py
@@ -23,6 +23,11 @@ def runOffline(args):
 
     delPreviousResults()
 
+    facesDetected = 0
+    confidence = 0
+    totalRuntime = 0
+    totalImages = len(os.listdir(folder))
+
     for filename in os.listdir(folder) :
         tic = utils.currentTime()
         
@@ -39,12 +44,48 @@ def runOffline(args):
         result.set_dateTime(utils.dateTime())
         result.set_runTime(round(toc-tic, 0))
 
+        facesDetected += len(result.get_faces())
+        totalRuntime += result.get_runTime()
+
+        if algorithm != 'haar' :
+
+            for conf in result.get_confidence() :
+                confidence += conf
+                # facesDetected += 1
+
+        
+
+
+
+
         # Save new image file
         with open(config.offlineOutputPath + '/' + filename, "wb") as fp:
             fp.write(result.get_img())
 
         result.set_img(f'{filename}')
         utils.appendJSON(config.offlineDataLocation, result)
+
+    
+
+    avgConfidence = round(confidence / facesDetected, 5)
+    avgFaces = (facesDetected / totalImages)
+    avgRuntime = (totalRuntime / totalImages)
+
+    if algorithm == 'haar' :
+        avgConfidence = 'na'
+
+    f = open(config.offlineLatexLocation, 'w+')
+    # latexString = algorithm + '&' + totalRuntime + '&' + avgRuntime + '&' + 
+    table = [algorithm, str(totalRuntime), str(avgRuntime), str(facesDetected), str(avgFaces), str(avgConfidence)]
+    tempStr = ''
+    for data in table :
+        tempStr += data + '&'
+        print(tempStr)
+
+    tempStr = tempStr[:-1] + r' \\'
+    f.write(tempStr)
+    
+    f.close()
 
     print('Marked images saved to ' + config.offlineOutputPath)
     print('JSON test results data saved to ' + config.offlineDataLocation)
